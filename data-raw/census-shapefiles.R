c1d91b2842f6c3abf2b55c5a0a0d0f4ef9ed8e73
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-07-22 16:58:00 -0400

    Move datasets from Census and USAboundaries
    This package will now contain all of the datasets.

--- data-raw/census-shapefiles.R
+++ data-raw/census-shapefiles.R
@@ -1,18 +1,13 @@
 library(plyr)
 library(rgdal)
 library(devtools)
+library(stringr)
 library(dplyr)
 
 baseurl <- "http://www2.census.gov/geo/tiger/GENZ2014/shp/"
-zipfiles <- c("cb_2014_us_state_500k.zip",
-              "cb_2014_us_state_5m.zip",
-              "cb_2014_us_county_500k.zip",
-              "cb_2014_us_county_5m.zip",
-              "cb_2014_us_cd114_500k.zip",
-              "cb_2014_us_cd114_5m.zip"
-              # "cb_2014_us_zcta510_500k.zip",
-              # "cb_2014_us_ua10_500k.zip"
-              )
+zipfiles <- c("cb_2014_us_state_20m.zip",
+              "cb_2014_us_county_20m.zip",
+              "cb_2014_us_cd114_20m.zip")
 
 l_ply(zipfiles, function(x) {
   url <- paste0(baseurl, x)
@@ -35,35 +30,24 @@ shapefiles <- gsub("data-raw/", "", shapefiles)
 
 l_ply(shapefiles, function(x) {
   shp <- readOGR("data-raw", x)
-  names(shp) <- tolower(names(shp))
+  names(shp@data) <- tolower(names(shp@data))
+#   is_utf <- sapply(shp@data, is.factor)
+#   shp@data[is_utf] <- lapply(shp@data[is_utf],
+#                              stri_trans_general, "latin-ascii")
   shp@data[] <- lapply(shp@data, iconv, "UTF-8", "ASCII")
   assign(x, shp, envir = globalenv())
 })
 
-
-state_geoid <- cb_2014_us_state_5m@data %>%
+state_geoid <- cb_2014_us_state_20m@data %>%
   select(statefp, name) %>%
   rename(state_name = name) %>%
   distinct()
 
-cb_2014_us_county_5m@data <- cb_2014_us_county_5m@data %>%
+cb_2014_us_county_20m@data <- cb_2014_us_county_20m@data %>%
   left_join(state_geoid, by = c("statefp" = "statefp"))
 
-cb_2014_us_county_500k@data <- cb_2014_us_county_500k@data %>%
+cb_2014_us_cd114_20m@data <- cb_2014_us_cd114_20m@data %>%
   left_join(state_geoid, by = c("statefp" = "statefp"))
 
-cb_2014_us_cd114_5m@data <- cb_2014_us_cd114_5m@data %>%
-  left_join(state_geoid, by = c("statefp" = "statefp"))
-
-cb_2014_us_cd114_500k@data <- cb_2014_us_cd114_500k@data %>%
-  left_join(state_geoid, by = c("statefp" = "statefp"))
-
-use_data(cb_2014_us_county_500k,
-         cb_2014_us_county_5m,
-         cb_2014_us_state_500k,
-         cb_2014_us_state_5m,
-         cb_2014_us_ua10_500k,
-         cb_2014_us_cd114_5m,
-         cb_2014_us_cd114_500k,
-         # cb_2014_us_zcta510_500k,
+use_data(cb_2014_us_county_20m, cb_2014_us_state_20m, cb_2014_us_cd114_20m,
          compress = "xz", overwrite = TRUE)

5c7c3d61afd844a98b8099514a869d55917b95d4
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-06-19 14:28:30 -0400

    Clean up city columns

--- data-raw/historical-cities.R
+++ data-raw/historical-cities.R
@@ -1,6 +1,15 @@
 library(tidyverse)
 
 census_cities <- read_csv("data-raw/1790-2010_MASTER.csv") %>%
-  gather(year, population, 5:27, convert = TRUE)
+  gather(year, population, 5:27, convert = TRUE) %>%
+  select(-ID, - CityST) %>%
+  select(city = City, state = ST, everything()) %>%
+  filter(population > 0) %>%
+  mutate(STPLFIPS_2010 =
+           if_else(STPLFIPS_2010 == "0", NA_character_, STPLFIPS_2010)) %>%
+  mutate(Name_2010 =
+           if_else(Name_2010 == "0", NA_character_, Name_2010))
+
+colnames(census_cities) <- tolower(colnames(census_cities))
 
 devtools::use_data(census_cities, overwrite = TRUE, compress = "xz")

10e88b28c72cf43f32d0042ac34b55c6c693d344
Lincoln Mullen <lincoln@lincolnmullen.com>
2017-06-21 16:12:00 -0400

    Update census boundaries, add zipcodes, convert to sf objects

--- R/census-bureau.R
+++ R/census-bureau.R
@@ -3,28 +3,34 @@
 #' The U.S. Census Bureau provides
 #' \href{https://www.census.gov/geo/maps-data/data/tiger-cart-boundary.html}{cartographic
 #' boundary files} for current U.S. boundaries. The datasets in this package
-#' provide a selection of boundaries from the 2014 Census files. They are
-#' objects of class \code{\link[sp]{SpatialPolygonsDataFrame}}. They are
-#' intended to be used with the functions in the \code{USAboundaries} package,
-#' but the data objects can also be used on their own. The attributes data
-#' associated with these boundaries is unchanged from what is available in the
-#' Census boundaries, with the exception that a \code{state_name} column has
-#' been added where necessary for convience in filtering the boundaries.
+#' provide a selection of boundaries from the 2016 Census files. They are
+#' objects of class \code{sf} from the
+#' \href{https://cran.r-project.org/package=sf}{sf package}. They are intended
+#' to be used with the functions in the \code{USAboundaries} package, but the
+#' data objects can also be used on their own. The data attributes associated
+#' with these boundaries is unchanged from what is available in the Census
+#' boundary files, with the exception that \code{state_name} and
+#' \code{state_abbreviation} columns have been added where necessary for
+#' convience in filtering the boundaries.
 #'
-#' The following objects are included in this package or in \code{USAboundariesData}:
+#' The following objects are included in this package:
 #'
-#' \describe{
-#' \item{cb_2014_us_cd114_500k}{Congressional district boundaries for the 114th Congress, 1:500k resolution.}
-#' \item{cb_2014_us_cd114_20m}{Congressional district boundaries for the 114th Congress, 1:20m resolution.}
-#' \item{cb_2014_us_county_500k}{County boundaries, 1:500k resolution.}
-#' \item{cb_2014_us_county_20m}{County boundaries, 1:20m resolution.}
-#' \item{cb_2014_us_state_500k}{State boundaries, 1:500k resolution.}
-#' \item{cb_2014_us_state_20m}{State boundaries, 1:20m resolution.}
-#' }
+#' \describe{\item{congress_contemporary_hires}{Congressional district
+#' boundaries for the 115th Congress, 1:500k resolution.}
+#' \item{congress_contemporary_lores}{Congressional district boundaries for the
+#' 114th Congress, 1:20m resolution.} \item{counties_contemporary_hires}{County
+#' boundaries, 1:500k resolution.} \item{counties_contemporary_lores}{County
+#' boundaries, 1:20m resolution.} \item{states_contemporary_hires}{State
+#' boundaries, 1:500k resolution.} \item{zipcodes}{Centroids of Zip Code
+#' Tabulation Areas.} }
+#'
+#' See the \href{https://cran.r-project.org/package=USAboundaries}{USAboundaries
+#' package} for low-resolution state boundaries and for the functions to access
+#' this data.
 #'
 #' @references U.S. Census Bureau,
 #'   \href{https://www.census.gov/geo/maps-data/data/tiger-cart-boundary.html}{Cartographic
-#'    Boundary Shapefiles} (2014).
+#'    Boundary Shapefiles} (2017).
 #'
 #'   See the U.S. Census Bureau's
 #'   "\href{https://www.census.gov/geo/reference/geoidentifiers.html}{Understanding
@@ -35,26 +41,26 @@
 #' @rdname census_boundaries
 NULL
 
-#' @name   cb_2014_us_state_500k
+#' @name congress_contemporary_hires
 #' @rdname census_boundaries
 NULL
 
-#' @name   cb_2014_us_county_500k
+#' @name congress_contemporary_lores
 #' @rdname census_boundaries
 NULL
 
-#' @name   cb_2014_us_cd114_500k
+#' @name counties_contemporary_hires
 #' @rdname census_boundaries
 NULL
 
-#' @name cb_2014_us_cd114_20m
+#' @name counties_contemporary_lores
 #' @rdname census_boundaries
 NULL
 
-#' @name  cb_2014_us_county_20m
+#' @name states_contemporary_hires
 #' @rdname census_boundaries
 NULL
 
-#' @name  cb_2014_us_state_20m
+#' @name zipcodes
 #' @rdname census_boundaries
 NULL
--- data-raw/census-shapefiles.R
+++ data-raw/census-shapefiles.R
@@ -1,93 +1,72 @@
-library(plyr)
-library(rgdal)
-library(devtools)
+library(tidyverse)
+library(sf)
 library(stringr)
-library(dplyr)
+library(USAboundaries)
+library(devtools)
 
-baseurl <- "http://www2.census.gov/geo/tiger/GENZ2014/shp/"
-zipfiles <- c("cb_2014_us_state_20m.zip",
-              # "cb_2014_us_state_5m.zip",
-              "cb_2014_us_state_500k.zip",
-              "cb_2014_us_county_20m.zip",
-              # "cb_2014_us_county_5m.zip",
-              "cb_2014_us_county_500k.zip",
-              "cb_2014_us_cd114_20m.zip",
-              # "cb_2014_us_cd114_5m.zip",
-              "cb_2014_us_cd114_500k.zip")
+baseurl <- "http://www2.census.gov/geo/tiger/GENZ2016/shp/"
+zipfiles <- c("cb_2016_us_state_20m.zip",
+              "cb_2016_us_state_500k.zip",
+              "cb_2016_us_county_20m.zip",
+              "cb_2016_us_county_500k.zip",
+              "cb_2016_us_cd115_20m.zip",
+              "cb_2016_us_cd115_500k.zip",
+              "cb_2016_us_zcta510_500k.zip")
 
-l_ply(zipfiles, function(x) {
-  url <- paste0(baseurl, x)
-  file <- paste0("data-raw/", x)
+zip_dir <- "data-raw/temp/census-shapefiles/"
+dir.create(zip_dir, showWarnings = FALSE, recursive = TRUE)
+
+walk(zipfiles, function(x) {
+  url <- str_c(baseurl, x)
+  file <- str_c(zip_dir, x)
   if (!file.exists(file)) {
     message("Downloading ", x)
     download.file(url, file, mode = "wb")
   }
 })
 
-l_ply(zipfiles, function(x) {
-  file <- paste0("data-raw/", x)
+walk(zipfiles, function(x) {
+  file <- str_c(zip_dir, x)
   message("Unzipping ", x)
-  unzip(file, exdir = "data-raw")
+  unzip(file, exdir = zip_dir, overwrite = TRUE)
 })
 
-shapefiles <- Sys.glob("data-raw/*.shp")
-shapefiles <- gsub(".shp", "", shapefiles)
-shapefiles <- gsub("data-raw/", "", shapefiles)
+cleanup_shp <- function(f, join = TRUE) {
+  shp <- f %>%
+    str_c(zip_dir, .) %>%
+    st_read(stringsAsFactors = FALSE) %>%
+    st_transform(4326)
 
-l_ply(shapefiles, function(x) {
-  shp <- readOGR("data-raw", x)
-  names(shp@data) <- tolower(names(shp@data))
-  shp@data[] <- lapply(shp@data, iconv, "UTF-8", "ASCII")
-  assign(x, shp, envir = globalenv())
-})
+  if (join) {
+    shp <- shp %>%
+      left_join(USAboundaries::state_codes, by = c("STATEFP" = "state_code"))
+  }
 
-state_geoid <- cb_2014_us_state_20m@data %>%
-  select(statefp, name) %>%
-  rename(state_name = name) %>%
-  distinct()
+  colnames(shp) <- str_to_lower(colnames(shp))
+  shp
+}
 
-cb_2014_us_county_20m@data <- cb_2014_us_county_20m@data %>%
-  left_join(state_geoid, by = c("statefp" = "statefp"))
+states_contemporary_lores <- cleanup_shp("cb_2016_us_state_20m.shp")
+states_contemporary_hires <- cleanup_shp("cb_2016_us_state_500k.shp")
+counties_contemporary_lores <- cleanup_shp("cb_2016_us_county_20m.shp")
+counties_contemporary_hires <- cleanup_shp("cb_2016_us_county_500k.shp")
+congress_contemporary_lores <- cleanup_shp("cb_2016_us_cd115_20m.shp")
+congress_contemporary_hires <- cleanup_shp("cb_2016_us_cd115_500k.shp")
+zcta <- cleanup_shp("cb_2016_us_zcta510_500k.shp", join = FALSE)
+zipcodes <- zcta %>%
+  st_transform(3857) %>%
+  st_centroid() %>%
+  mutate(zipcode = zcta5ce10) %>%
+  select(zipcode, everything()) %>%
+  st_transform(4326)
 
-# cb_2014_us_county_5m@data <- cb_2014_us_county_5m@data %>%
-#   left_join(state_geoid, by = c("statefp" = "statefp"))
-
-cb_2014_us_county_500k@data <- cb_2014_us_county_500k@data %>%
-  left_join(state_geoid, by = c("statefp" = "statefp"))
-
-cb_2014_us_cd114_20m@data <- cb_2014_us_cd114_20m@data %>%
-  left_join(state_geoid, by = c("statefp" = "statefp"))
-
-# cb_2014_us_cd114_5m@data <- cb_2014_us_cd114_5m@data %>%
-#   left_join(state_geoid, by = c("statefp" = "statefp"))
-
-cb_2014_us_cd114_500k@data <- cb_2014_us_cd114_500k@data %>%
-  left_join(state_geoid, by = c("statefp" = "statefp"))
-
-# From http://www.udsmapper.org/zcta-crosswalk.cfm
-# zipcodes <- read.csv("data-raw/zcta_county_rel_10.csv", colClasses = "character")
-
-# zipcodes <- zipcodes %>%
-#   select(zcta5ce10 = ZCTA5, fips_state = STATE) %>%
-#   distinct(zcta5ce10)
-
-# state_codes <- read.csv("data-raw/state_codes.csv",
-#                         stringsAsFactors = FALSE,
-#                         colClasses = "character")
-
-# cb_2014_us_zcta510_500k@data <- cb_2014_us_zcta510_500k@data %>%
-#   left_join(zipcodes, by = "zcta5ce10") %>%
-#   select(1:5, state_code = fips_state) %>%
-#   left_join(state_codes, by = "state_code") %>%
-#   select(-state_code, -state_abbr, -jurisdiction_type)
-
-use_data(cb_2014_us_state_20m,
-         # cb_2014_us_state_5m,
-         cb_2014_us_state_500k,
-         cb_2014_us_county_20m,
-         # cb_2014_us_county_5m,
-         cb_2014_us_county_500k,
-         cb_2014_us_cd114_20m,
-         # cb_2014_us_cd114_5m,
-         cb_2014_us_cd114_500k,
+use_data(states_contemporary_hires,
+         states_contemporary_lores,
+         counties_contemporary_hires,
+         counties_contemporary_lores,
+         congress_contemporary_hires,
+         congress_contemporary_lores,
+         zipcodes,
          compress = "xz", overwrite = TRUE)
+
+file.rename("data/states_contemporary_lores.rda", "../USAboundaries/data/states_contemporary_lores.rda")

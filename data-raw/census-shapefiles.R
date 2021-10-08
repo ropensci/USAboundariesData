fc01b588a2fd78533313f0f37f972ec43f4b8d31
Lincoln Mullen <lincoln@lincolnmullen.com>
2021-10-08 13:33:37 -0400

    Update Census Bureau data

--- data-raw/census-shapefiles.R
+++ data-raw/census-shapefiles.R
@@ -4,29 +4,27 @@ library(stringr)
 library(USAboundaries)
 library(devtools)
 
-baseurl <- "http://www2.census.gov/geo/tiger/GENZ2016/shp/"
-zipfiles <- c("cb_2016_us_state_20m.zip",
-              "cb_2016_us_state_500k.zip",
-              "cb_2016_us_county_20m.zip",
-              "cb_2016_us_county_500k.zip",
-              "cb_2016_us_cd115_20m.zip",
-              "cb_2016_us_cd115_500k.zip",
-              "cb_2016_us_zcta510_500k.zip")
+zipfiles <- c("http://www2.census.gov/geo/tiger/GENZ2020/shp/cb_2020_us_state_20m.zip",
+              "http://www2.census.gov/geo/tiger/GENZ2020/shp/cb_2020_us_state_500k.zip",
+              "http://www2.census.gov/geo/tiger/GENZ2020/shp/cb_2020_us_county_20m.zip",
+              "http://www2.census.gov/geo/tiger/GENZ2020/shp/cb_2020_us_county_500k.zip",
+              "http://www2.census.gov/geo/tiger/GENZ2020/shp/cb_2020_us_cd116_20m.zip",
+              "http://www2.census.gov/geo/tiger/GENZ2020/shp/cb_2020_us_cd116_500k.zip",
+              "http://www2.census.gov/geo/tiger/GENZ2019/shp/cb_2019_us_zcta510_500k.zip")
 
 zip_dir <- "data-raw/temp/census-shapefiles/"
 dir.create(zip_dir, showWarnings = FALSE, recursive = TRUE)
 
-walk(zipfiles, function(x) {
-  url <- str_c(baseurl, x)
-  file <- str_c(zip_dir, x)
+walk(zipfiles, function(url) {
+  file <- str_c(zip_dir, basename(url))
   if (!file.exists(file)) {
-    message("Downloading ", x)
+    message("Downloading ", url)
     download.file(url, file, mode = "wb")
   }
 })
 
 walk(zipfiles, function(x) {
-  file <- str_c(zip_dir, x)
+  file <- str_c(zip_dir, basename(x))
   message("Unzipping ", x)
   unzip(file, exdir = zip_dir, overwrite = TRUE)
 })
@@ -46,13 +44,13 @@ cleanup_shp <- function(f, join = TRUE) {
   shp
 }
 
-states_contemporary_lores <- cleanup_shp("cb_2016_us_state_20m.shp")
-states_contemporary_hires <- cleanup_shp("cb_2016_us_state_500k.shp")
-counties_contemporary_lores <- cleanup_shp("cb_2016_us_county_20m.shp")
-counties_contemporary_hires <- cleanup_shp("cb_2016_us_county_500k.shp")
-congress_contemporary_lores <- cleanup_shp("cb_2016_us_cd115_20m.shp")
-congress_contemporary_hires <- cleanup_shp("cb_2016_us_cd115_500k.shp")
-zcta <- cleanup_shp("cb_2016_us_zcta510_500k.shp", join = FALSE)
+states_contemporary_lores <- cleanup_shp("cb_2020_us_state_20m.shp")
+states_contemporary_hires <- cleanup_shp("cb_2020_us_state_500k.shp")
+counties_contemporary_lores <- cleanup_shp("cb_2020_us_county_20m.shp")
+counties_contemporary_hires <- cleanup_shp("cb_2020_us_county_500k.shp")
+congress_contemporary_lores <- cleanup_shp("cb_2020_us_cd116_20m.shp")
+congress_contemporary_hires <- cleanup_shp("cb_2020_us_cd116_500k.shp")
+zcta <- cleanup_shp("cb_2019_us_zcta510_500k.shp", join = FALSE)
 zipcodes <- zcta %>%
   st_transform(3857) %>%
   st_centroid() %>%
@@ -69,4 +67,4 @@ use_data(states_contemporary_hires,
          zipcodes,
          compress = "xz", overwrite = TRUE)
 
-file.copy("data/states_contemporary_lores.rda", "../USAboundaries/data/states_contemporary_lores.rda")
+file.copy("data/states_contemporary_lores.rda", "../USAboundaries/data/states_contemporary_lores.rda", overwrite = TRUE)
